077af6a0752f2627078c8628a3fc377b2aa9b542
Youssuf-bakry <youssuf.bakry@gmail.com>
2023-10-27 07:53:23 +0300

    adding command.sh

---
+++ src/command.sh
@@ -0,0 +1,11 @@
+cd "E:\New folder 2\Yosef_Programming\reactProjects" 
+npx create-react-app git-blackbird
+git reset --hard 
+git commit -am "updating logo which is invalid though"
+git merge update_logo 
+git status
+git switch update_logo 
+git push -u "https://github.com/Youssuf-bakry/git-blackbird.git" update_logo
+git push -u "https://github.com/Youssuf-bakry/git-blackbird.git" master 
+
+https://github.com/Youssuf-bakry/git-blackbird
